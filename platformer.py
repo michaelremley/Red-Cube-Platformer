12df633516c81d4242abb8643ebc29102ce5d296
Michael Remley <mremley@olin.edu>
2019-03-25 16:43:22 -0400

    Update platformer.py description

--- platformer.py
+++ platformer.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-This is a worked example of applying the Model-View-Controller (MVC)
-design pattern to the creation of a simple arcade game (in this case
-Brick Breaker).
+This is a platformer game based on keyboard contol for our Software Design project.
 
-We will create our game in stages so that you can see the process by
-which the MVC pattern can be utilized to create clean, extensible,
-and modular code.
-
-@author: SoftDesProfs
+@author: Michael Remley and Skye Ozga
 """
 
 import time

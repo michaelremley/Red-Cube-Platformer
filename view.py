dfa0b165abadfa536ec4703016ef6bde610472c4
Michael Remley <mremley@olin.edu>
2019-04-01 23:21:27 -0400

    Add documentation and write-up

--- view.py
+++ view.py
@@ -13,11 +13,15 @@ class PyGameWindowView(object):
             specified game screen dimensions (represented as a tuple
             containing the width and height """
         self.model = model
+        # Make display at resolution size in full screen
         self.screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
 
     def draw(self):
-        """ Draw the current game state to the screen """
+        """ Draw the current game objects to the screen """
+        # Black background
         self.screen.fill(pygame.Color(0,0,0))
+
+        # Draw white platforms in model
         for platform in self.model.platforms:
             pygame.draw.rect(self.screen,
                              pygame.Color(255, 255, 255),
@@ -25,6 +29,7 @@ class PyGameWindowView(object):
                                          platform.y,
                                          platform.width,
                                          platform.height))
+        # Draw avatar as red square
         pygame.draw.rect(self.screen,
                          pygame.Color(255, 0, 0),
                          pygame.Rect(self.model.avatar.x-self.model.left_edge,
@@ -33,4 +38,5 @@ class PyGameWindowView(object):
                                      self.model.avatar.height))
         #print(self.model.avatar.x)
         #print(self.model.left_edge)
+        # Call update so things actually change
         pygame.display.update()
